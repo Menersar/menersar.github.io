afc5838638da6b405f18766b62840e50f51fb62d
Menersar <menersar@menersar.com>
2023-08-07 00:44:56 +0200

    add updated and rebuild files

--- Sidekick/packager/sw.js
+++ Sidekick/packager/sw.js
@@ -1,7 +1,7 @@
 // These will be replaced at build-time by generate-service-worker-plugin.js
-const ASSETS = ["","js/download-project.worker.js","assets/reset.80a6e1615fc013684ad8047dba5ce064.svg","assets/default-icon.290e09e569a1cab8e61ba93b0d23863f.png","js/icns.js","js/jszip.js","js/p4.js","js/packager-options-ui.js","js/sha256.js","js/vendors~icns~jszip.js","js/vendors~icns~jszip~sha256.js"];
-const CACHE_NAME = "p4-69765a50fd4651866aac196b0da70b698b46ace95dec82f889bc6f9583f4a2d7";
-const IS_PRODUCTION = false;
+const ASSETS = ["","js/download-project.6f0e2ce799895edeb2be.worker.js","js/p4.04f8a2e74ce123fc880d.js"];
+const CACHE_NAME = "p4-2edd2b5d2f239113af7d27c902e7c5fd23d46dc54a99ce5f1da88240a12b94d9";
+const IS_PRODUCTION = true;
 
 const base = location.pathname.substr(0, location.pathname.indexOf('sw.js'));
 
