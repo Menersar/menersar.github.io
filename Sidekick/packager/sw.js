82fa21a6d01b3dee52d2051182031938dede8bbc
Menersar <menersar@menersar.com>
2023-08-09 04:56:38 +0200

    upload rebuilt packager

--- Sidekick/packager/sw.js
+++ Sidekick/packager/sw.js
@@ -1,6 +1,6 @@
 // These will be replaced at build-time by generate-service-worker-plugin.js
-const ASSETS = ["","js/download-project.d130e9436a87688a16c6.worker.js","assets/reset.80a6e1615fc013684ad8047dba5ce064.svg","assets/default-icon.290e09e569a1cab8e61ba93b0d23863f.png","js/vendors~icns~jszip~sha256.45d79f17e909e6d47b0e.js","js/icns.ddff1f925efd18aed929.js","js/jszip.41ecc138b10fe9350a55.js","js/p4.e73f039216ee0f441369.js","js/packager-options-ui.26422e7727f8abaf0ca9.js","js/sha256.65a53bbd4b259520122b.js"];
-const CACHE_NAME = "p4-b993c54665135b33102ef319a5493a2b1c1a9e098381cc51604f3a618bc4ec7c";
+const ASSETS = ["","js/download-project.307880fa10fd08800082.worker.js","assets/reset.1b7c2728718a119273c903df95ab4c2a.svg","assets/default-icon.290e09e569a1cab8e61ba93b0d23863f.png","js/vendors~icns~jszip~sha256.45d79f17e909e6d47b0e.js","js/icns.ddff1f925efd18aed929.js","js/jszip.41ecc138b10fe9350a55.js","js/p4.eb9de8d6b953daae66d9.js","js/packager-options-ui.d8badbd52948b5935482.js","js/sha256.65a53bbd4b259520122b.js"];
+const CACHE_NAME = "p4-5518c37dd1f7d41c95f90a9cee041c221064ef7c2195de6df1c1000423cf7e8e";
 const IS_PRODUCTION = true;
 
 const base = location.pathname.substr(0, location.pathname.indexOf('sw.js'));
